c0ab3b2bb442e2209faf6879adea309384b5329a
lollipop-onl <lollipop@simochee.net>
2025-05-28 11:29:26 +0900

    chore: tailwindcss

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 
 export default defineConfig({
-	manifest: {
-		default_locale: "ja",
-		name: "__MSG_ext_name__",
-		description: "__MSG_ext_description__",
-	},
-	modules: ["@wxt-dev/i18n/module", "@wxt-dev/auto-icons"],
+  manifest: {
+    default_locale: "ja",
+    name: "__MSG_ext_name__",
+    description: "__MSG_ext_description__",
+  },
+  modules: ["@wxt-dev/i18n/module", "@wxt-dev/auto-icons"],
+  vite: () => ({
+    plugins: [
+      // not compatible with Vite 6 types
+      tailwindcss() as any,
+    ],
+  }),
 });
